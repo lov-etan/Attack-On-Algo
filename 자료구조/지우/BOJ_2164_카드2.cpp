fd78a108a2cb0801a0734196b34044ecd91cda1e
지우 <emily1290@naver.com>
2025-05-29 12:04:56 +0900

    Solved: [자료구조] BOJ_카드2 홍지우

---
+++ 자료구조/지우/BOJ_2164_카드2.cpp
@@ -0,0 +1,32 @@
+// 메모리: 4040 KB 시간: 4 ms
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int N; int answer = 1; // 1일 때는 while에 들어가지 못한다..!!!
+    cin >> N;
+
+    queue<int> q;
+
+    for(int i=1; i<=N; i++) {
+        q.push(i);
+    }
+
+    while(q.size() > 1) {         
+        q.pop(); // 제일 위에 걸 버린다
+
+        int num = q.front(); // 그 다음, 제일 위에 있는 카드를 제일 아래로 옮긴다.
+        q.pop();
+        q.push(num);
+        
+        answer = q.front(); //현재 남아있는 수 확인
+    }
+
+    cout << answer;
+}
